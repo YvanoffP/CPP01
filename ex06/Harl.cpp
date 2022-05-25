b33095e6690d31cb4cf94ee038fe418fd97e3c3e
YvanoffP <petruzzi.yvan@gmail.com>
2022-05-25 03:47:08 +0200

    damnit Harl, it's 4am. Matt Dirty is proud

---
+++ ex06/Harl.cpp
@@ -0,0 +1,68 @@
+#include "Harl.hpp"
+
+typedef void ( *Func ) ();
+
+Harl::Harl( void )
+{
+    return ;
+}
+
+Harl::~Harl( void )
+{
+    return ;
+}
+
+void Harl::debug( void )
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info( void )
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void )
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error( void )
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain( std::string level )
+{
+    std::string cringe[] = {
+        "debug",
+        "info",
+        "warning",
+        "error",
+    };
+
+    void (Harl::*funcs[4])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+
+    for (size_t i = 0; i < sizeof(cringe); i++)
+    {
+        if (cringe[i] == level)
+        {
+            while (i < 4)
+            {
+                std::cout << "[ ";
+                for (size_t j = 0; j < cringe[i].length(); j++)
+                    std::cout << (char)std::toupper(cringe[i][j]);
+                std::cout << " ]" << std::endl;
+                (this->*funcs[i++])();
+                std::cout << "\n";
+            }
+            return ;
+        }
+    }
+    std::cout << "Harl is not inspired about this.." << std::endl;
+}
---
+++ ex06/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main( int argc, char **argv )
+{
+    if (argc != 2)
+    {
+        std::cout << "Send only one argument" << std::endl;
+        return (1);
+    }
+    Harl harl;
+    harl.complain(argv[1]);
+    return (0);
+}
