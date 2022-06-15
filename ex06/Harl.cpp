512caac562a5017060517a261aef02ce5758d1b4
YvanoffP <petruzzi.yvan@gmail.com>
2022-06-15 11:43:25 +0200

    changed file according to the correction pdf

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -14,31 +14,35 @@ Harl::~Harl( void )
 
 void Harl::debug( void )
 {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info( void )
 {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning( void )
 {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error( void )
 {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain( std::string level )
 {
     std::string cringe[] = {
-        "debug",
-        "info",
-        "warning",
-        "error",
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
     };
 
     void (Harl::*funcs[4])(void) = {
@@ -48,21 +52,17 @@ void Harl::complain( std::string level )
         &Harl::error
     };
 
-    for (size_t i = 0; i < sizeof(cringe); i++)
+    int i;
+    for (i = 0; i < 4 && level != cringe[i]; i++);
+    switch (i)
     {
-        if (cringe[i] == level)
-        {
-            while (i < 4)
-            {
-                std::cout << "[ ";
-                for (size_t j = 0; j < cringe[i].length(); j++)
-                    std::cout << (char)std::toupper(cringe[i][j]);
-                std::cout << " ]" << std::endl;
-                (this->*funcs[i++])();
-                std::cout << "\n";
-            }
-            return ;
-        }
+        case 0 : (this->*funcs[0])();
+		case 1 : (this->*funcs[1])();
+		case 2 : (this->*funcs[2])();
+		case 3 : (this->*funcs[3])();
+			break;
+        default:
+			std::cout << "[ Probable complaining about insignificant problems ]" << std::endl;
+			break;
     }
-    std::cout << "Harl is not inspired about this.." << std::endl;
 }
